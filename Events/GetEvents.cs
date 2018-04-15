ccc902d8dae80e70250e5feb54633450e56cf800
Nicolai Søndergaard <nicolai.sondergaard@gmail.com>
2018-04-15 13:45:14 +0200

    Cleanup.

--- Events/GetEvents.cs
+++ Events/GetEvents.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -18,26 +17,22 @@ namespace Events
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            IEventsRepository eventsRepository = new EventsRepository();
+            try
+            {
+                IEventsRepository eventsRepository = new EventsRepository();
 
-            //if (req.Method == HttpMethod.Get)
-            //{
-            //    // Simple get, return all events.
-            //    List<IEvent> events = eventsRepository.GetEvents();
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                List<IEvent> events = eventsRepository.GetEvents((string)data?.category, (IAddress)data?.location, (DateTime?)data?.fromDate, (DateTime?)data?.toDate);
 
-            //    return req.CreateResponse(HttpStatusCode.OK, events);
-            //}
-            // Get request body
-            dynamic data = await req.Content.ReadAsAsync<object>();
-            //name = data?.name;
-            List<IEvent> events = eventsRepository.GetEvents((string)data?.category, (IAddress)data?.location, (DateTime?)data?.fromDate, (DateTime?)data?.toDate);
-
-            return req.CreateResponse(HttpStatusCode.OK, events);
-
-            //return name == null
-            //    ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
-            //    : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
-            return req.CreateResponse(HttpStatusCode.OK, "lala");
+                return req.CreateResponse(HttpStatusCode.OK, events);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error", ex, "GetEvents.cs");
+                return req.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+            
         }
     }
 }
